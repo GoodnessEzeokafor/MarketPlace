da509d4c07fb5c4fe79249e27db9e5d15fb2e2e2
Goodness Ezeokafor <gootech442@yahoo.com>
2020-01-10 15:28:23 +0100

    project created

--- src/contracts/MarketPlace.sol
+++ src/contracts/MarketPlace.sol
@@ -36,7 +36,7 @@ contract MarketPlace{
     uint public buyerCount =0;
     uint public transportCompanyCount =0;
     uint public traceCount =0;
-    
+    uint public productCount = 0;
     mapping (uint=>Farmer) public farmers;
     mapping (uint=>Buyer) public buyers;
     mapping (uint=>TransportCompany) public transport_companies;
@@ -84,9 +84,7 @@ contract MarketPlace{
         string product_description;
         uint product_price;
         string upload_image;
-        bool sold;
         address payable seller;
-        uint price;
     }
 
     struct Trace{
@@ -131,9 +129,7 @@ contract MarketPlace{
         string product_description,
         uint product_price,
         string upload_image,
-        bool sold,
-        address payable seller,
-        uint price
+        address payable seller
     );
     event TraceCreated(
         uint id,
@@ -157,6 +153,9 @@ contract MarketPlace{
         uint price  
     );
     event BoughtProduct(address _from,address _to,uint amt);
+    event Left(uint id, bool left, uint updated);
+    event Arrived(uint id, bool arrived, uint updated);
+    
     /* END EVENT */
     /* START METHOD */
     function createSellerProfile(
@@ -231,6 +230,16 @@ contract MarketPlace{
             msg.sender,
             _price
         );
+
+        emit TransportCompanyCreated(
+            transportCompanyCount,
+                _transport_name,
+                _transport_description,
+                _picture,
+                _transport_address,
+                msg.sender,
+                _price
+        );
     }
 
     function createTrace(
@@ -251,21 +260,143 @@ contract MarketPlace{
             0
         );
     }
-        // uint price_id;
-        // string  product_name;
-        // uint product_price;
 
-        //     uint id;
-        // uint product_id;
-        // uint transport_company_id;
-        // bool left;
-        // bool arrived;
-        // bool pending; 
-        // uint timestamp;
-        // uint updated;
+    function leftTrace(
+        uint _id,
+        bool status
+    )public{
+        Trace memory trace = tracing[_id];
+        trace.left = status;
+        trace.updated = now;
+        tracing[_id] = trace;
+        emit Left(trace.id,trace.left,trace.updated);
+    }
+    function arriveTrace(
+        uint _id,
+        bool status
+    )public{
+        Trace memory trace = tracing[_id];
+        trace.arrived = status;
+        trace.updated = now;
+        tracing[_id] = trace;
+        emit Arrived(trace.id,trace.arrived,trace.updated);
+    }
 
 
-    /* END METHOD */
-    
 
+    function getFarmerDetail(uint _id)public view returns(
+                                  uint id,
+                                  string memory,
+                                  string memory,
+                                  string memory,
+                                  string memory,
+                                  string memory,
+                                  address
+    ){
+        Farmer memory f = farmers [_id];
+        return (
+            f.id,
+            f.full_name,
+            f.picture,
+            f.farm_name,
+            f.email_adddress,
+            f.farm_description,
+            f.farmer_wallet_address
+        );
+    }
+
+
+    function getBuyerDetail(uint _id)public view returns(
+                uint,
+        string  memory,
+        string memory,
+        string memory ,
+        address
+    ){
+        Buyer memory b = buyers [_id];
+        return(
+            b.id,
+            b.full_name,
+            b.picture,
+            b.email_address,
+            b.buyer_wallet_address
+        );
+    }
+
+
+    function getTransportCompanyDetaul(uint _id)public view returns(
+        uint id,
+        string memory,
+        string memory,
+        string memory, 
+        string memory,
+        address,
+        uint
+    ){
+        TransportCompany memory t = transport_companies [_id];
+        return (
+            t.id,
+            t.transport_name,
+            t.transport_description,
+            t.picture,
+            t.transport_address,
+            t.transport_wallet_address,
+            t.price
+        );
+    }   
+
+
+
+    function createProduct(
+        string memory _product_name,
+        string memory _product_description,
+        uint _price,
+        string memory _upload_image
+    )public{
+        productCount++;
+        products[productCount]= Product(
+            productCount,
+            _product_name,
+            _product_description,
+            _price,
+            _upload_image,
+            msg.sender
+        );
+        emit ProductCreated(
+              productCount,
+            _product_name,
+            _product_description,
+            _price,
+            _upload_image,
+            msg.sender
+        );
+    }
+
+    function buyProduct(uint _id) public payable{
+        // fetch the product
+                // Product memory p = products[_id];
+
+        Product memory _product = products[_id];
+        // fetch the owner
+        address payable _seller = _product.seller;
+        // make sure the product is is valid
+        require(_product.id > 0 && _product.id <= productCount,"NOT A PRODUCT ID");
+        // require that there is enough Ether in the transaction
+        require(msg.value >= _product.product_price,"You Don't Have Enough Ether");
+        // require that the buyer is not the seller
+        require(_seller != msg.sender,"Seller Can't Purchase His Item");
+        // transfer ownership
+        // _product.owner = msg.sender;
+        // mark as purchased
+        //update the product
+        products[_id] = _product;
+        // pay the seller by paying them ether
+        address(_seller).transfer(msg.value); 
+        // trigger an event
+        emit BoughtProduct(
+            msg.sender,   
+            _seller,
+            msg.value
+        );
+    }
 }
