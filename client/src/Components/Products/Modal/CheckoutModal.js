59efa56a817dcefa46deb9fddb25e24b9b25371b
Goodness Ezeokafor <gootech442@yahoo.com>
2020-01-30 08:18:37 +0100

    made changes

--- client/src/Components/Products/Modal/CheckoutModal.js
+++ client/src/Components/Products/Modal/CheckoutModal.js
@@ -52,7 +52,7 @@ export default class CheckoutModal extends Component {
                                                 className="form-control" name="" id="">
                                             {this.props.transport_companies.map((transport, key) => {
                                                 return(
-                                                <option id="{transport.id}" value="PRESIDENT">{transport.transport_name}</option>
+                                                <option key={key} id="{transport.id}" value="PRESIDENT">{transport.transport_name}</option>
                                                 );
                                             })}
                                            </select>
